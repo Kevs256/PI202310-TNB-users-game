e14dc309eba1feff46276abdd7ad88687e2b01cd
diegomated1 <diegocardwr@gmail.com>
2023-04-30 18:59:33 -0500

    update interfaces

--- src/interfaces/IDeck.ts
+++ src/interfaces/IDeck.ts
@@ -7,5 +7,6 @@ export interface IDeckProducts{
 	id_deck_products?: number
     id_product: string
     quantity: number
+    type: string
     id_deck: number
 }
---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,5 @@
+export interface IProduct {
+    id_product: string
+    quantity: number
+    type: string
+}
