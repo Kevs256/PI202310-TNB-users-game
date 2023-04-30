4756f275e5ad27cb07b3445f26cb823b96d3a808
diegomated1 <diegocardwr@gmail.com>
2023-04-30 13:20:17 -0500

    models interfaces

---
+++ src/interfaces/IDeck.ts
@@ -0,0 +1,11 @@
+export interface IDeck{
+	id_deck: number
+    id_user: string
+}
+
+export interface IDeckProducts{
+	id_deck_products: number
+    id_product: string
+    quantity: number
+    id_deck: number
+}
