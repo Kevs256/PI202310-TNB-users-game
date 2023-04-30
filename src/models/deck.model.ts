3ae28749e66e98157677c90ee2afd15117ad5a7a
diegomated1 <diegocardwr@gmail.com>
2023-04-30 13:19:31 -0500

    change database and models

---
+++ src/models/deck.model.ts
@@ -0,0 +1,42 @@
+import {Model, DataTypes, BuildOptions} from 'sequelize';
+import db from '../database/database.js';
+import { IDeck, IDeckProducts } from 'interfaces/IDeck.js';
+
+interface DeckInstance extends Model<IDeck>, IDeck {}
+type DeckModelStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): DeckInstance;
+};
+
+interface DeckProductsInstance extends Model<IDeckProducts>, IDeckProducts {}
+type DeckProductsModelStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): DeckProductsInstance;
+};
+
+export const deckModel = db.define('deck', {
+    id_deck: {
+      primaryKey: true,
+      type: DataTypes.NUMBER,
+      autoIncrement: true
+    },
+    id_user: DataTypes.STRING
+}, {
+    freezeTableName: true,
+    timestamps: false
+}) as DeckModelStatic;
+
+export const deckProductsModel = db.define('deckProducts', {
+    id_deck_products: {
+        primaryKey: true,
+        type: DataTypes.NUMBER,
+        autoIncrement: true
+    },
+    id_product: DataTypes.STRING,
+    quantity: DataTypes.NUMBER,
+    id_deck: DataTypes.NUMBER,
+}, {
+  freezeTableName: true,
+  timestamps: false
+}) as DeckProductsModelStatic;
+
+deckModel.hasMany(deckProductsModel, { foreignKey: 'id_deck' });
+deckProductsModel.belongsTo(deckModel, { foreignKey: 'id_deck' });
--- src/models/inventory.model.ts
+++ src/models/inventory.model.ts
@@ -1,13 +1,43 @@
-import mongoose from 'mongoose';
+import {Model, DataTypes, BuildOptions} from 'sequelize';
+import db from '../database/database.js';
+import { IInventory, IInventoryProducts } from 'interfaces/IInventory.js';
 
-const InventoryModel = mongoose.model('Inventory', new mongoose.Schema({
-    id_user: String,
-    product: [
-        {
-          id_product: { type: String, required: true },
-          quantity: { type: Number, required: true }
-        }
-      ],
-}));
+interface InventoryInstance extends Model<IInventory>, IInventory {}
+type InventoryModelStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): InventoryInstance;
+};
 
-export default InventoryModel;
+interface InventoryProductsInstance extends Model<IInventoryProducts>, IInventoryProducts {}
+type InventoryProductsModelStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): InventoryProductsInstance;
+};
+
+export const inventoryModel = db.define('inventory', {
+    id_inventory: {
+      primaryKey: true,
+      type: DataTypes.NUMBER,
+      autoIncrement: true
+    },
+    id_user: DataTypes.STRING,
+    coins: DataTypes.NUMBER
+}, {
+    freezeTableName: true,
+    timestamps: false
+}) as InventoryModelStatic;
+
+export const inventoryProductsModel = db.define('inventoryProducts', {
+  id_inventory_products: {
+    primaryKey: true,
+    type: DataTypes.NUMBER,
+    autoIncrement: true
+  },
+  id_product: DataTypes.STRING,
+  quantity: DataTypes.NUMBER,
+  id_inventory: DataTypes.NUMBER,
+}, {
+  freezeTableName: true,
+  timestamps: false
+}) as InventoryProductsModelStatic;
+
+inventoryModel.hasMany(inventoryProductsModel, { foreignKey: 'id_inventory' });
+inventoryProductsModel.belongsTo(inventoryModel, { foreignKey: 'id_inventory' });
